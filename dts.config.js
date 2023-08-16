510b41db5a02cbfa32d31006a62f78da12d72e2a
Phung <phungnlg@gmail.com>
2023-08-16 21:45:19 +0700

    update config

--- dts.config.js
+++ dts.config.js
@@ -1,16 +1,17 @@
 const url = require('@rollup/plugin-url');
+const copy = require('rollup-plugin-copy');
 
 module.exports = {
   rollup(config) {
     config.plugins = [
         ...config.plugins,
         url({
-            // Where to put files
-            destDir: 'dist/assets/',
-            // Path to put infront of files (in code)
-            publicPath: 'src/',
-            // File name once copied
-            fileName: '[name][extname]',
+            // // Where to put files
+            // destDir: 'dist/assets/',
+            // // Path to put infront of files (in code)
+            // publicPath: 'src/',
+            // // File name once copied
+            // fileName: '[name][extname]',
             // Kinds of files to process
             include: [
                 '**/*.svg',
@@ -18,8 +19,14 @@ module.exports = {
                 '**/*.gif',
                 '**/*.jpg',
                 '**/*.jpeg',
+                '**/*.js',
             ]
         }),
+        copy({
+            targets: [
+              { src: 'src/shim.js', dest: 'dist/' },
+            ]
+        })
     ];
 
     return config;
